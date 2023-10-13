88f2e91dd074e8a51d54eaac0c4dd96410d7e35a
RogerAgu <bobjackson298@gmail.com>
2023-10-12 20:20:26 -0500

    main page layout

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,5 +2,36 @@
 </script>
 
 <template>
-   <h1 class="text-6xl font-thin text-slate-800">I'm Home Page</h1>
+ <div class="brand">
+    <img class="logo" src="logo.svg" alt="logo" />
+    <h1 class="title">Triviantastic</h1>
+    <img class="logo" src="logo.svg" alt="logo" />
+  </div>
+  <div class="categories">
+   <RouterLink  :key="n" :to="`question/category/${n}`" v-for="n in 24" class="category">Category - {{ n }}</RouterLink>
+   </div>
 </template>
+
+<style lang="postcss" scoped>
+.brand{
+   @apply flex items-center justify-center gap-4;
+
+   & .logo{
+      @apply h-16 w-16;
+   }
+
+   &.title{
+      @apply text-5xl font-thin tracking-widest text-slate-900;
+   }
+}
+.categories {
+   @apply grid flex-grow grid-cols-4 gap-12;
+& .category {
+    @apply flex h-32 items-center justify-center rounded-lg border-4 border-green-500 py-4 text-center font-bold uppercase text-slate-600 transition-colors duration-300;
+
+    &:hover {
+      @apply cursor-pointer border-red-500 bg-red-500 text-white;
+    }
+  }
+}
+</style>
