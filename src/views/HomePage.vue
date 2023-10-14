cb06b7f29fd83a644ac2632adf25d1744e3848b8
RogerAgu <bobjackson298@gmail.com>
2023-10-13 22:25:34 -0500

    added category once call

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,33 +1,36 @@
 <script setup>
-import { onMounted, ref } from 'vue'
+import { onMounted } from 'vue'
+import useAPI from '@/composables/useApi'
 import BaseTitle from '@/components/BaseTitle.vue'
-import useApi from '@/composables/useApi'
-const api = useApi()
-const categories = ref([])
+
+const { categories, getCategories } = useAPI()
 
 onMounted(async () => {
-  categories.value = await api.getCategories()
+  await getCategories()
 })
 </script>
 
 <template>
-   <!--  -->
+  <!--  -->
   <BaseTitle>
     <template #logo>
-      <img src="logo.svg" alt="logo" />
+      <img src="/logo.svg" alt="logo" />
     </template>
     Triviantastic
   </BaseTitle>
-  <div class="categories">
-   <RouterLink  :key="category.id" :to="`question/category/${category.id}`" v-for="category in categories" class="category"> {{ category.name }}</RouterLink>
-   </div>
- 
+  <div v-if="categories.length > 0" class="categories">
+    <RouterLink v-for="category in categories" :key="category.id" :to="`/question/category/${category.id}`"
+      class="category">
+      {{ category.name }}
+    </RouterLink>
+  </div>
 </template>
 
 <style lang="postcss" scoped>
 .categories {
-   @apply grid flex-grow grid-cols-4 gap-12;
-& .category {
+  @apply grid flex-grow grid-cols-4 gap-12;
+
+  & .category {
     @apply flex h-32 items-center justify-center rounded-lg border-4 border-green-500 py-4 text-center font-bold uppercase text-slate-600 transition-colors duration-300;
 
     &:hover {
@@ -35,6 +38,4 @@ onMounted(async () => {
     }
   }
 }
-
-
 </style>
