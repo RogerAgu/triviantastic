6b5162a33a77f04259f52150fba0f212973c14b6
RogerAgu <bobjackson298@gmail.com>
2023-10-13 22:17:23 -0500

    added answer logic

--- src/views/QuestionPage.vue
+++ src/views/QuestionPage.vue
@@ -1,44 +1,64 @@
 <script setup>
 import BaseTitle from '@/components/BaseTitle.vue';
 import { onMounted, ref } from 'vue'
+import { shuffle } from 'lodash-es'
 import { useRoute } from 'vue-router';
 import useApi from '@/composables/useApi';
 import useColor from "@/composables/useColor";
+import useScore from '@/composables/useScore';
+import DifficultyChip from '@/components/DifficultyChip.vue'
+import NotificationAnswers from '@/components/NotificationAnswers.vue'
 
 const api = useApi()
 const route = useRoute();
 const question = ref(null)
 const answers = ref([])
 const colors = useColor()
+const showNotification = ref(false)
+const isCorrect = ref(false)
+const { changeScore } = useScore()
+const handleAnswer = (points) => {
+  isCorrect.value = points > 0
+  showNotification.value = true
+  setTimeout(() => {
+    changeScore(points)
+    router.push('/')
+  }, 1000)
+}
+
 onMounted(async () => {
   question.value = await api.getQuestion(route.params.id)
   answers.value.push({
     id: answers.value.length,
     correct: true,
     answer: question.value.correct_answer,
+    points: question.value.difficulty === 'easy' ? 10 : question.value.difficulty === 'medium' ? 20 : 30,
   })
   question.value.incorrect_answers.map((answer) => {
     answers.value.push({
       id: answers.value.length,
       correct: false,
       answer,
+      points: -5,
     })
   })
+  answers.value = shuffle(answers.value)
 })
 
 </script>
 
 <template>
-<div v-if="question" class="question-container">
-  <BaseTitle>{{ question.category }} </BaseTitle>
-  <p class="question">{{ question.question }}</p>
- <div class="answers">
-      <div v-for="answer in answers" :key="answer.id" :class="colors.getColor(answer.id)" class="answer">
-        {{ answer.answer }}
-      </div>
+  <div v-if="question" class="question-container">
+    <BaseTitle> {{ question.category }}</BaseTitle>
+    <p class="question" v-html="question.question" />
+    <div class="answers">
+      <div v-for="answer in answers" :key="answer.id" :class="colors.getColor(answer.id)" class="answer"
+        @click="handleAnswer(answer.points)" v-html="answer.answer" />
     </div>
- </div>
+    <DifficultyChip :difficulty="question.difficulty" />
+  </div>
   <div v-else class="loading">Loading...</div>
+  <NotificationAnswers v-if="showNotification" :correct="isCorrect" />
 </template>
 
 <style lang="postcss" scoped>
@@ -53,7 +73,11 @@ onMounted(async () => {
     @apply grid w-full flex-grow grid-cols-2 gap-8;
 
     & .answer {
-      @apply flex items-center justify-center rounded-lg text-center text-4xl;
+      @apply flex items-center justify-center rounded-lg text-center text-4xl text-white;
+
+      &:hover {
+        @apply cursor-pointer;
+      }
     }
   }
 }
